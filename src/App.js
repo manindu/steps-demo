a88c3f2245a8cb71fa324fda166e63da2ed6fbb4
Manindu Wijewickrama <manindu90@gmail.com>
2019-03-06 23:06:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
+import { Flipper, Flipped } from 'react-flip-toolkit';
 import logo from './logo.svg';
 import './App.css';
+import styles from './styles.module.scss';
+
+const StepItem = ({ number }) => {
+  return (
+    <div className={styles.step}>
+      <div className={styles.line}/>
+      <div className={styles.circle}>
+        <p>{number}</p>
+      </div>
+      <div className={styles.line}/>
+    </div>
+  );
+};
+
+class Steps extends Component {
+  getItems = () => {
+    const { steps, currentStep } = this.props;
+    const items = steps.map(step => <StepItem number={step} />)
+
+    return items;
+  };
+
+  render () {
+    const { currentStep } = this.props;
+    return (
+
+        <div className={styles.stepGroup}>
+          <Flipped flipId={currentStep}>
+            <div className={styles.currentCircle} />
+          </Flipped>
+          {this.getItems()}
+        </div>
+    );
+  }
+}
 
 class App extends Component {
+
+  state = {
+    current: 1
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+          <Steps steps={[1, 2, 3, 4, 5]} currentStep={this.state.current}/>
+        </div>
+        <div>
+          <button onClick={() => this.setState({ current: this.state.current - 1 })}>Previos</button>
+          <button onClick={() => this.setState({ current: this.state.current + 1 })}>Next</button>
+        </div>
       </div>
     );
   }
